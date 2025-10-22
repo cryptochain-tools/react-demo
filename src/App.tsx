43fa930ccafa2687010ed17eae13fb3dcd46aa2b
soren.huang <soren.huang@lbk.one>
2025-10-22 21:21:43 +0800

    feat:add

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + 改了一次</h1>
+      <h1>Vite + React + 改了2次</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
